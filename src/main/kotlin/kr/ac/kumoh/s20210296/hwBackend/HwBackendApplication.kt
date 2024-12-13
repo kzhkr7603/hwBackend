169733a6d81325bbbc92316681b91b22d77971ab
kzhkr <kzhkr@naver.com>
2024-12-13 19:05:34 +0900

    Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210296/hwBackend/HwBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210296.hwBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HwBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<HwBackendApplication>(*args)
+}
