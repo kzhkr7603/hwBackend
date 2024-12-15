32088803470999b0b160986ae3bb3ce0bd2b5ec4
kzhkr <kzhkr@naver.com>
2024-12-15 23:09:27 +0900

    CORS 추가

--- src/main/kotlin/kr/ac/kumoh/s20210296/hwBackend/controller/MessageController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210296/hwBackend/controller/MessageController.kt
@@ -2,14 +2,15 @@ package kr.ac.kumoh.s20210296.hwBackend.controller
 
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/message")
+@CrossOrigin(origins = ["https://hwfrontend.netlify.app"])
 class MessageController {
+
     @GetMapping
-    // TODO: React 주소 변경할 것
-    @CrossOrigin(origins = ["http://localhost:3000"])
+    fun getRoot() = mapOf("status" to "success")
+
+    @GetMapping("/api/message")
     fun getMessage() = mapOf("status" to "success")
 }
